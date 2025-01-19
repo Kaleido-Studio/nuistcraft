81b2e5186df3a46e539b4e39ad2d8fe567d8cddb
HowardZhangdqs <zjh@shanghaiit.com>
2025-01-19 20:29:43 +0800

    feat: add markdown support for contraption note and usage

--- src/components/CoordDisplay.tsx
+++ src/components/CoordDisplay.tsx
@@ -1,16 +1,37 @@
 /* eslint-disable react-dom/no-dangerously-set-innerhtml */
+import { marked } from 'marked'
 import React from 'react'
 import data from './CoordData'
 import styles from './CoordDisplay.module.css'
 
 type axis = number | string | undefined
 type Coord = [axis, axis, axis]
-export interface Equipment {
+export interface Contraptions {
   /** 装置名称 */
   name: string
 
-  /** 装置图标 */
+  /**
+   * 备注
+   *
+   * 可以使用 Markdown 格式
+   */
+  note?: string
+
+  /**
+   * 装置图标
+   *
+   * 如果是字符串，则为图片地址
+   * 如果是字符串数组，则为图片地址数组，用于轮播
+   */
   avatar?: string[] | string
+
+  /**
+   * 装置图片
+   *
+   * 如果是字符串，则为图片地址
+   * 如果是字符串数组，则为图片地址数组，用于轮播
+   */
+  image?: string[] | string
 
   /** 主世界坐标 */
   world?: Coord
@@ -22,15 +43,16 @@ export interface Equipment {
   end?: Coord
 
   /** 作者 */
-  author?: string[]
+  author?: string[] | string
 
   /** 损坏请联系 */
-  fix?: string[]
+  fix?: string[] | string
 
-  /** 备注 */
-  note?: string
-
-  /** 使用方法 */
+  /**
+   * 使用方法
+   *
+   * 可以使用 Markdown 格式
+   */
   usage?: string
 
   /** 是否需要自备材料 */
@@ -96,6 +118,7 @@ function ImageDisplay({ src }: { src: string[] }) {
 function CoordDisplayCard({
   name,
   avatar,
+  image,
   world,
   nether,
   end,
@@ -103,7 +126,7 @@ function CoordDisplayCard({
   fix,
   note,
   usage,
-}: Equipment) {
+}: Contraptions) {
   return (
     <div className={styles.card}>
       <div style={{ width: 'calc(100% - var(--ifm-h2-font-size) * 1.5)', textAlign: 'right' }}>
@@ -117,8 +140,25 @@ function CoordDisplayCard({
       <p className={`${styles.title} ${styles.p}`}>
         {name}
       </p>
-      {note && <p className={styles.p} dangerouslySetInnerHTML={{ __html: note }}></p>}
-
+      {note && <p className={styles.p} dangerouslySetInnerHTML={{ __html: marked.parse(note) }}></p>}
+      {image && (
+        Array.isArray(image)
+          ? (image.map(element => (
+              <img
+                className={styles.display_img}
+                src={element}
+                alt=""
+                key={element}
+              />
+            )))
+          : (
+              <img
+                className={styles.display_img}
+                src={Array.isArray(image) ? image[0] : image}
+                alt=""
+              />
+            )
+      )}
       {world && (
         <p className={styles.p}>
           主世界坐标：
@@ -152,21 +192,21 @@ function CoordDisplayCard({
       {author && (
         <p className={styles.p}>
           作者：
-          {author.join('，')}
+          {Array.isArray(author) ? author.join('，') : author}
         </p>
       )}
       {fix && (
         <p className={styles.p}>
           损坏请联系：
-          {fix.join('，')}
+          {Array.isArray(fix) ? fix.join('，') : fix}
         </p>
       )}
-      {usage && <p className={styles.p} dangerouslySetInnerHTML={{ __html: usage }}></p>}
+      {usage && <p className={styles.p} dangerouslySetInnerHTML={{ __html: marked.parse(usage) }}></p>}
     </div>
   )
 }
 
-function CoordDisplay({ equipments }: { equipments: Equipment[] }) {
+function CoordDisplay({ equipments }: { equipments: Contraptions[] }) {
   return (
     <div className={styles.grid}>
       {equipments.map(equipment => (
